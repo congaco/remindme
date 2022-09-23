784f7211e69604d77b5b156df666507dc765ff24
Van <nguyenpha@cip.ifi.lmu.de>
2022-09-23 10:55:47 +0200

    implemented functions for displaying notification window after an amount of time

--- src/view/AppView.py
+++ src/view/AppView.py
@@ -6,8 +6,11 @@ class AppView(tkinter.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.notification = None
+
+
         self.message_verify = None
-        self.labelMessage = tkinter.Label(self, text="Message: ") # save messages in combox for more messages
+        self.labelMessage = tkinter.Label(self, text="Message: ")  # save messages in combox for more messages
         self.labelMessage.grid(row=0, column=0)
 
         self.message_var = tkinter.StringVar()
@@ -34,10 +37,32 @@ class AppView(tkinter.Frame):
 
     def display_notification(self, message, time):
         notification = tkinter.Tk()
-        notification.after(time, exit)
+        notification.after_idle(time, self.appear())
         notificationGUI = tkinter.Label(notification, text=message, font=("Arial", 12), bg="green")
         notificationGUI.pack()
         return notificationGUI
+
+    def set_message_notification(self):
+        tkinter.Label(self.notification, text=self.controller.read_content("messages"), font=("Arial", 12),
+                      bg="green").place(x=10, y=10)
+
+    def disappear(self):
+        self.set_message_notification()
+        self.notification.withdraw()
+        if self.time_var.get().isdigit():
+            self.notification.after(int(self.time_var.get()), self.appear)
+
+    def appear(self):
+        self.set_message_notification()
+        self.notification.deiconify()
+        if self.time_var.get().isdigit():
+            self.notification.after(int(self.time_var.get()), self.disappear)
+
+    def start(self):
+        self.notification = tkinter.Tk()
+        tkinter.Frame(self.notification, width=250, height=100).pack()
+        self.notification.after_idle(self.appear)
+        self.notification.mainloop()
 
     def is_activate_button_clicked(self):
         if self.controller:
@@ -49,7 +74,7 @@ class AppView(tkinter.Frame):
                 self.set_verified_message("green")
                 self.message_verify.grid(row=2, column=1, sticky=tkinter.EW)
                 self.verify_var.set("Verified!")
-                self.display_notification(self.controller.read_content("messages"), self.time_var.get()).mainloop()
+                self.start()
 
     def set_verified_message(self, color):
         self.message_verify = tkinter.Label(self, textvariable=self.verify_var, width=100, foreground=color)
